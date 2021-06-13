dfc4da6d4004f499d4ed1884693e39263bed95a6
Eric <eric.bisbjerg@hotmail.com>
2021-06-13 23:28:32 +0200

    Added first display function

---
+++ script.js
@@ -0,0 +1,13 @@
+function showIntro() {
+    hideAll();
+    document.getElementById("intro").classList = "paragraf";
+}
+
+
+
+
+function hideAll() {
+    document.getElementById("intro").classList = "hide";
+    document.getElementById("about").classList = "hide";
+    document.getElementById("resume").classList = "hide";
+}
